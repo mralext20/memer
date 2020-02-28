ae89d6906472f03e2079c4a76a9c3075167e6d71
Alexander Terry <alexterry43@gmail.com>
2020-02-28 13:53:33 -0700

    add comment schema to dbcontext

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 import ValueSchema from "../models/Value";
 import ProfileSchema from "../models/Profile";
 import MemeSchema from "../models/Meme";
+import CommentSchema from "../models/Comment"
 class DbContext {
   Values = mongoose.model("Value", ValueSchema);
   Memes = mongoose.model("Memes", MemeSchema);
+  Comments = mongoose.model("Comments", CommentSchema)
   Profile = mongoose.model("Profile", ProfileSchema);
 }
 
