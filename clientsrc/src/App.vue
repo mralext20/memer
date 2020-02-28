09b22a3653a09bf2e53f23f5f76ca130f5c66fc6
ZachDickson <z.dickson10@gmail.com>
2020-02-28 14:42:03 -0700

    add post form

--- clientsrc/src/App.vue
+++ clientsrc/src/App.vue
@@ -2,15 +2,23 @@
   <div id="app">
     <navbar />
     <router-view />
-
-    <div class="text-center text-success">
-      <h1>Memer</h1>
-      <i class="far fa-plus-square"></i>
+    <div class="container-fluid">
+      <div class="text-center text-success">
+        <h1>Memer</h1>
+        <div class="row sort-form mx-2">
+          <h3>sort</h3>
+          <i @click="toggleForm" class="far fa-plus-square"></i>
+        </div>
+      </div>
+      <div class="row justify-content-center">
+        <createPost v-if="formToggle == true" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import createPost from "./components/CreatePost";
 import Navbar from "@/components/navbar";
 import { onAuth } from "@bcwdev/auth0-vue";
 export default {
@@ -23,7 +31,19 @@ export default {
     }
   },
   components: {
-    Navbar
+    Navbar,
+    createPost
+  },
+  methods: {
+    toggleForm() {
+      this.formToggle = !this.formToggle;
+    }
+  },
+
+  data() {
+    return {
+      formToggle: false
+    };
   }
 };
 </script>
@@ -36,5 +56,9 @@ i {
 
 body {
   background-color: rgb(56, 55, 55);
+}
+
+.sort-form {
+  justify-content: space-between;
 }
 </style>
