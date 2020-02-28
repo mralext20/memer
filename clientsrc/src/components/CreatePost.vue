982deaa5a4e0e60b58bb18caa1a395c2eb35eb65
Alexander Terry <alexterry43@gmail.com>
2020-02-28 15:58:48 -0700

    creating posts

--- clientsrc/src/components/CreatePost.vue
+++ clientsrc/src/components/CreatePost.vue
@@ -2,10 +2,10 @@
   <div class="col-5">
     <form @submit.prevent="createPost">
       <div class="form-group">
-        <input type="text" class="form-control" id="imgUrl" placeholder="Img Url" />
+        <input type="text" class="form-control" id="imgUrl" v-model="meme.memeUrl" />
       </div>
       <div class="form-group">
-        <input type="text" class="form-control" id="title" placeholder="Title" />
+        <input type="text" class="form-control" id="title" v-model="meme.title" />
       </div>
       <div class="form-check"></div>
       <button type="submit" class="btn btn-primary">Submit</button>
@@ -17,7 +17,18 @@
 <script>
 export default {
   name: "CreatePost",
-  props: ["toggleForm"]
+  props: ["toggleForm"],
+  data() {
+    return {
+      meme: {}
+    };
+  },
+  methods: {
+    createPost() {
+      this.$store.dispatch("addMeme", this.meme);
+      this.meme = {};
+    }
+  }
 };
 </script>
 
